76dd63e545e53253db600d68493f90abbbdd2792
KatrinNekhin <77107757+KatrinNekhin@users.noreply.github.com>
2021-11-10 20:42:15 +0200

    Update enc_parity_8bit.v
    tested

--- encoder_decoder/encoder_decoder_lib/hdl/enc_parity_8bit.v
+++ encoder_decoder/encoder_decoder_lib/hdl/enc_parity_8bit.v
@@ -10,9 +10,16 @@
 
 `resetall
 `timescale 1ns/10ps
-module enc_parity_8bit ;
-
-
-// ### Please start your Verilog code here ### 
+module enc_parity_8bit (	
+  input 	wire[3:0] 	data_in,
+	output 	reg[3:0]	parity_8
+);
+  
+	always @ (data_in) begin
+		parity_8[0] <= ^data_in[3:1];
+		parity_8[1] <= ^data_in[2:0];
+		parity_8[2] <= ^data_in[1:0] ^ data_in[3];
+		parity_8[3] <= data_in[0] ^ ^data_in[3:2];
+	end
 
 endmodule
